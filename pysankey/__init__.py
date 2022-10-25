e1b014bd81a8bcc73fa381c238a90a372d9fe471
Raimondas <raimondas.zemblys@gmail.com>
2022-10-25 21:03:20 +0200

    Update __init__.py

--- pysankey/__init__.py
+++ pysankey/__init__.py
@@ -1,6 +1,1 @@
 
-""" Make simple, pretty Sankey Diagrams """
-
-from pysankey.sankey import sankey, PySankeyException, NullsInFrame, LabelMismatch
-
-__all__ = ["sankey", "PySankeyException", "NullsInFrame", "LabelMismatch"]
